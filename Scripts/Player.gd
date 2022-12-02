8a5419c96d94b228bd32e0b8852cad40a2a4d1c1
hennifant <henning.schwichow@gmx.de>
2022-12-02 16:39:00 +0100

    Added health and mana bar

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,14 @@ extends KinematicBody2D
 # Player movement speed
 export var speed = 75
 
+# Player stats
+var health = 100
+var health_max = 100
+var health_regeneration = 1
+var mana = 100
+var mana_max = 100
+var mana_regeneration = 2
+
 # Animation variables
 var last_direction = Vector2(0, 1)
 var attack_playing = false
@@ -10,6 +18,23 @@ var attack_playing = false
 # Player dragging flag
 var drag_enabled = false
 
+signal player_stats_changed
+
+func _ready():
+	emit_signal("player_stats_changed", self)
+
+func _process(delta):
+	# Regenerates mana
+	var new_mana = min(mana + mana_regeneration * delta, mana_max)
+	if new_mana != mana:
+		mana = new_mana
+		emit_signal("player_stats_changed", self)
+	
+	# Regenerates health
+	var new_health = min(health + health_regeneration * delta, health_max)
+	if new_health != health:
+		health = new_health
+		emit_signal("player_stats_changed", self)
 
 func _physics_process(delta):
 	# Get player input
@@ -61,9 +86,12 @@ func _input(event):
 		var animation = get_animation_direction(last_direction) + "_attack"
 		$Sprite.play(animation)
 	elif event.is_action_pressed("fireball"):
-		attack_playing = true
-		var animation = get_animation_direction(last_direction) + "_fireball"
-		$Sprite.play(animation)
+		if mana >= 25:
+			mana = mana - 25
+			emit_signal("player_stats_changed", self)
+			attack_playing = true
+			var animation = get_animation_direction(last_direction) + "_fireball"
+			$Sprite.play(animation)
 
 
 func get_animation_direction(direction: Vector2):
